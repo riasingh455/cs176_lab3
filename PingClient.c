46f4a03785de73b03ddc3c04192fabce19745c30
riasingh455 <riasingh@ucsb.edu>
2025-11-08 19:53:31 -0800

    few

--- PingClient.c
+++ PingClient.c
@@ -82,9 +82,10 @@ int main(int argc, char *argv[]) {
     double avg_rtt = (received > 0) ? sum_rtt / received : 0;
 
     printf("--- %s ping statistics ---\n", server_name);
-    printf("%d packets transmitted, %d received, %d%% packet loss\n", sent, received, (int)loss);
+    printf("%d packets transmitted, %d received, %.0f%% packet loss\n", sent, received, loss);
     if (received > 0)
-    printf("rtt min/avg/max = %.3f %.3f %.3f ms\n", min_rtt, avg_rtt, max_rtt);
+        printf("rtt min/avg/max = %.3f %.3f %.3f ms\n", min_rtt, avg_rtt, max_rtt);
+
 
 
     close(sockfd);
